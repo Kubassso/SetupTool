f9d2d752b5972ce4bd9d1109f06ef901948ac5f1
Kubassso_to_szef <254238975+Kubassso@users.noreply.github.com>
2026-03-27 14:45:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let files = [];
+
+fetch("manifest.json")
+  .then(res => res.json())
+  .then(data => {
+    files = data;
+    render(files);
+  });
+
+function render(list) {
+  const container = document.getElementById("fileList");
+  container.innerHTML = "";
+
+  list.forEach(file => {
+    const div = document.createElement("div");
+    div.className = "file";
+    div.innerHTML = `
+      <h3>${file.name}</h3>
+      <a href="${file.file}" download>Pobierz</a>
+    `;
+    container.appendChild(div);
+  });
+}
+
+document.getElementById("search").addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+
+  const filtered = files.filter(f =>
+    f.name.toLowerCase().includes(value)
+  );
+
+  render(filtered);
+});
